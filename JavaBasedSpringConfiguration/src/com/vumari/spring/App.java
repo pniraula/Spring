340320770b45f3aba08d3dc6f26794a1d50360fe
pniraula <prabeshniraula@gmail.com>
2014-04-29 23:15:44 -0400

    Java Based Spring Configuration

---
+++ JavaBasedSpringConfiguration/src/com/vumari/spring/App.java
@@ -0,0 +1,14 @@
+package com.vumari.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(
+				HelloWorldConfig.class);
+		HelloWorld helloWorld = ctx.getBean(HelloWorld.class);
+
+		System.out.println(helloWorld.getMessage());
+	}
+}
